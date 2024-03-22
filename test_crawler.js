79cda2a02458f05409a441c4aa871a09e829d673
jin <jin.kolteh@gmail.com>
2024-03-22 09:45:22 +0800

    01

--- test_crawler.js
+++ test_crawler.js
@@ -3,20 +3,22 @@ const { dbQuery,dbInsert,dbUpdata,dbDelete } = require('./plugin/db')
 const { stockCrawler,stockCrawler_market,sleep } = require("./plugin/stockCrawler");
 //爬蟲股票
 async function crawlerStock(){
-  //跑個股
+  //個股
   const rows = await dbQuery( 'SELECT * from stock' )
   if(!rows){console.log(`crawlerStock失敗跳出`)}
   for (const [index, row] of rows.entries()) {
-  // for (const row of rows) {
     //跑股票
     await stockCrawler(row)
-    
-    //只抓2筆
-    // if(index>=1){break;}
 
     //等
     await sleep(20000)
   }
+
+  // market 大盤
+  const rows2 = await dbQuery( 'SELECT * from market where id=1' )
+  if(!rows2){console.log(`crawlerStock_market失敗跳出`)}
+  // console.log(`rows2,${JSON.stringify(rows2[0])}`)
+  await stockCrawler_market(rows2[0])
 }
 crawlerStock()
 
